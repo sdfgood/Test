d1c14289e10af5989d3457a21476e08d0e5722e4
Andrew <sdfgood@programist.ru>
2011-09-03 16:58:32 +0300

    1 Signed-off-by: Andrew <sdfgood@programist.ru>

---
+++ Reshaem.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("Reshaem.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Reshaem.Tests")]
+[assembly: AssemblyCopyright("© , 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID-идентификации библиотеки типов, если этот проект видим для COM
+[assembly: Guid("e02c80d9-b1c8-4bce-9f1e-1ffa6869594c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
